c60b4142296e92c2ac16cdec8aff76298ba89c52
Snihar <16243@ecam.be>
2018-10-07 11:20:03 +0200

    first add

---
+++ C#_cours_until_31-09-2018/Mod1_lab1/Mod1_lab1/bin/Debug/coursinfo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod1_lab1
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            //initialize variables for student information
+            string firstName = "";
+            string lastName = "";
+            DateTime birthday;
+            string adress = "";
+            string city = "";
+            int postal = 0;
+            string country = "";
+            string degree = "";
+
+            //intialize variables course information
+            string courseName = "";
+            int credits = 0;
+            int numberOfHours = 0;
+            string TeacherName = "";
+
+            //assign values to those variables
+            lastName = "Snyers ";
+            firstName = "Harold ";
+            birthday = new DateTime(1998, 6, 23);
+            adress = "avenue du polo 9";
+            city = "Brussels";
+            postal = 1150;
+            country = "Belgium";
+            degree = "3rd year of bachelor";
+            courseName = "Electronics";
+            credits = 4;
+            numberOfHours = 24;
+            TeacherName = "Marchand";
+
+            //some console.wright
+            Console.WriteLine("My name is {0}{1}. My birthdate is {2}.", lastName, firstName, birthday);
+            Console.WriteLine("my adress is {0} {1} {2} {3}", adress, postal, city, country);
+            Console.WriteLine($"I'm doing my {degree} this year, 2018-2019");
+            Console.WriteLine("One of my courses is {0}, It's given by {1} and has a value of " +
+                "{2} credits. We will have {3} hours of this cours", courseName, TeacherName, credits, numberOfHours);
+        }
+    }
+}
---
+++ C#_cours_until_31-09-2018/Mod2_lab4/Mod2_lab4/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod2_lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a while loop
+            // We start with n = 1
+            // The condition check for the while, tests if n is less than 6, if so, the loop body code executes
+            // inside the loop, we output the value of n and then increment it by 1
+            // Once n = 6, the loop stops
+            // Pay attention to the output to see what the last value is to ensure you understand
+            // how the evaluation of the condition is done and how the while loop executes
+            int m = 1;
+            while (m < 6)
+            {
+                Console.WriteLine($"Current value of m is {m}");
+                m++;
+            }
+
+            // Create a do loop, also known as a do..while loop
+            // Note that with the do loop, the loop will run at least once regardless of the value of x
+            // which is due to the condition not being checked until the end.
+            // Experiment with this by setting x to a value greater than 5 and run the code
+            int x = 0;
+            do
+            {
+                Console.WriteLine(x);
+                x++;
+            } while (x < 5);
+
+            // creating a chessboard
+            int n = 1;
+            while (n <= 8)
+            {
+                if (n % 2 == 0)
+                {
+                    Console.WriteLine("0X0X0X0X\r");
+                    Console.WriteLine("");
+                }
+                else
+                {
+                    Console.WriteLine("X0X0X0X0\r");
+                    Console.WriteLine("");
+                }
+                n++;
+            }
+
+
+
+        }
+    }
+}
